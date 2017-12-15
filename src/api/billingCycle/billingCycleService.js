20a8ab96fe7cdd041a2dee36d70884acebdcf939
matheus <freeuser153>
2017-12-14 22:14:53 -0200

    Did count

--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -3,5 +3,15 @@ const BillingCycle = require('./billingCyle');
 BillingCycle.methods(['get', 'post', 'put', 'delete']);
 BillingCycle.updateOptions({ new: true, runValidators: true });
 
+BillingCycle.route('count', (req, res, next) => {
+  BillingCycle.count((err, value) => {
+    if(err){
+      res.status(500).json({errors: [error]})
+    } else {
+      res.json({ value })
+    }
+  });
+})
+
 module.exports = BillingCycle;
 
